17d846d4fb66155bd948b4e4b9318efe0478b9be
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-10-30 09:55:12 -0700

    Started work on the View Model

---
+++ ProjectTalon.App/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ProjectTalon.App
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddTalonProjectServices(this IServiceCollection service)
+        {
+            service.AddTransient<IMnemonicService, MnemonicService>(); 
+            
+            return service;
+        }
+    }
+}
---
+++ ProjectTalon.App/ViewModel/RestoreViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CardanoSharp.Wallet.Enums;
+using CardanoSharp.Wallet.Models.Keys;
+using Microsoft.Maui.Controls;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ProjectTalon.App.ViewModel
+{
+    public class RestoreViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public Command RestoreWallet { get; set; }
+
+        public RestoreViewModel()
+        {
+
+        }
+
+        private Mnemonic RestoreMnemonic(string mnemonicPharase)
+        {
+            // Restore a Mnemonic
+            var mnemonic = new MnemonicService().Restore(mnemonicPharase);
+
+            // Fluent derivation API
+            var derivation = mnemonic
+                .GetMasterNode("password")      
+                .Derive(PurposeType.Shelley)    
+                .Derive(CoinType.Ada)           
+                .Derive(0)                      
+                .Derive(RoleType.ExternalChain) 
+                .Derive(0);                     
+
+            PrivateKey privateKey = derivation.PrivateKey;
+            PublicKey publicKey = derivation.PublicKey;
+        }
+    }
+}
