e0bc726270d4c21ddb4598706d30b97e0d65c59f
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-10-30 20:55:56 -0700

    Added view model updates

--- ProjectTalon.App/ViewModel/RestoreViewModel.cs
+++ ProjectTalon.App/ViewModel/RestoreViewModel.cs
@@ -8,9 +8,9 @@ using ProjectTalon.App.Data;
 using ProjectTalon.App.Data.Models;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using SQLiteNetExtensions.Extensions;
 using SQLite;
 using System.Threading.Tasks;
+using ProjectTalon.App.View;
 
 namespace ProjectTalon.App.ViewModel
 {
@@ -20,41 +20,82 @@ namespace ProjectTalon.App.ViewModel
 
         private readonly IMnemonicService _mnemonicService;
         private readonly WalletDatabase _walletDatabase;
-        readonly SQLite.SQLiteAsyncConnection database;
+        readonly SQLiteAsyncConnection database;
+        private readonly WalletKeyDatabase _walletKeyDatabase;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
         public Command RestoreWallet { get; set; }
 
-        public RestoreViewModel(IMnemonicService mnemonicService, WalletDatabase walletDatabase)
+        public RestoreViewModel(IMnemonicService mnemonicService, WalletDatabase walletDatabase, WalletKeyDatabase walletKeyDatabase)
         {
             _mnemonicService = mnemonicService;
             _walletDatabase = walletDatabase;
+            _walletKeyDatabase = walletKeyDatabase;
         }
 
-        private async Task<Mnemonic> RestoreMnemonicHDWalletAsync(string mnemonicPharase, string walletName)
+        private async Task<WalletView> RestoreMnemonicHDWalletAsync(string mnemonicPharase, string walletName)
         {
             // Restore a Mnemonic
             var mnemonic = _mnemonicService.Restore(mnemonicPharase);
 
             PrivateKey rootKey = mnemonic.GetRootKey();
+            Wallet newlyCreatedWallet; 
 
-            // This path will give us our Payment Key on index 0
-            string paymentPath = $"m/1852'/1815'/0'/0/0";
-            // The paymentPrv is Private Key of the specified path.
-            PrivateKey paymentPrv = rootKey.Derive(paymentPath);
-            // Get the Public Key from the Private Key
-            PublicKey paymentPub = paymentPrv.GetPublicKey(false);
 
-            // This path will give us our Stake Key on index 0
-            string stakePath = $"m/1852'/1815'/0'/2/0";
-            // The stakePrv is Private Key of the specified path
-            PrivateKey stakePrv = rootKey.Derive(stakePath);
-            // Get the Public Key from the Stake Private Key
-            PublicKey stakePub = stakePrv.GetPublicKey(false);
 
-            await database.RunInTransactionAsync((SQLiteConnection transaction))
+            await database.RunInTransactionAsync(async (SQLiteConnection transaction) =>
+            {
+                transaction.BeginTransaction();
+
+                var walletId = await _walletDatabase.SaveWalletAsync(new Wallet
+                {
+                    Name = walletName,
+                    WalletType = (int)WalletType.HD,
+                });
+
+                newlyCreatedWallet = await  _walletDatabase.GetWalletAsync(walletId);
+
+
+
+                // Creates the account keys for the wallet
+                string paymentPath = $"m/1852'/1815'/0'/0/0";
+                PrivateKey paymentPrv = rootKey.Derive(paymentPath);
+                PublicKey paymentPub = paymentPrv.GetPublicKey(false);
+                await _walletKeyDatabase.SaveWalletAsync(new WalletKey
+                {
+                    WalletId = newlyCreatedWallet.Id,
+                    KeyType = (int)KeyType.Account,
+                    Skey = paymentPub.Key.ToString(),
+                    Vkey = paymentPrv.Key.ToString(),
+                    KeyIndex = 0,
+
+                });
+
+
+                // Creates the staking keys for the wallet
+                string stakePath = $"m/1852'/1815'/0'/2/0";
+                PrivateKey stakePrv = rootKey.Derive(stakePath);
+                PublicKey stakePub = stakePrv.GetPublicKey(false);
+                await _walletKeyDatabase.SaveWalletAsync(new WalletKey
+                {
+                    WalletId = newlyCreatedWallet.Id,
+                    KeyType = (int)KeyType.Staking,
+                    Skey = stakePub.Key.ToString(),
+                    Vkey = stakePrv.Key.ToString(),
+                    KeyIndex = 0,
+
+                });
+
+                transaction.Commit();
+            });
+
+            return new WalletView
+            {
+                Name = walletName,
+                WalletType = (int)WalletType.HD,
+            }; 
         }
     }
 }
