dc38fbed9d312a04e526c4f92d4259d9d7ef9a59
Kyle Johns <kjohns1337@gmail.com>
2021-11-24 08:55:07 -0500

    updated and added new view models based on figma

---
+++ ProjectTalon.App/ViewModel/GenerateMnemonicViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CardanoSharp.Wallet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTalon.App.ViewModel
+{
+    public interface IGenerateMnemonicViewModel
+    {
+        string GenerateMnemonicAsync();
+    }
+    public class GenerateMnemonicViewModel : IGenerateMnemonicViewModel
+    {
+        private IMnemonicService _mnemonicService;
+
+        public GenerateMnemonicViewModel(IMnemonicService mnemonicService)
+        {
+            _mnemonicService = mnemonicService;
+        }
+
+        public string GenerateMnemonicAsync()
+        {
+            return _mnemonicService.Generate(24).Words;
+        }
+    }
+}
