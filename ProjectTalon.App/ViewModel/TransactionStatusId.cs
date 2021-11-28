a3efceb28a56e63413beccb954d38f345097d7d1
Vince <50725598+cvincentcoleman@users.noreply.github.com>
2021-11-28 07:05:08 -0700

    working on front end

---
+++ ProjectTalon.App/ViewModel/TransactionStatusId.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTalon.App.ViewModel
+{
+    internal enum TransactionStatusId
+    {
+        Sent = 0,
+        Recieved = 1,
+    }
+}
