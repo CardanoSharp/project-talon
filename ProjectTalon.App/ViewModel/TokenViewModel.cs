873e5d4698cdd7af9f4ace5b49af495ceb3396e2
Vince <50725598+cvincentcoleman@users.noreply.github.com>
2021-12-13 07:47:39 -0500

    Began work on send page

---
+++ ProjectTalon.App/ViewModel/TokenViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTalon.App.ViewModel
+{
+    public class TokenViewModel
+    {
+        public string Name { get; set; }
+        public string Asset { get; set; }
+        public int Amount { get; set; }
+    }
+}
