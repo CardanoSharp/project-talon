de077f7b0848a6dd608ed6fa396096a49c9062dc
Vince <50725598+cvincentcoleman@users.noreply.github.com>
2021-12-14 08:58:12 -0500

    started working on receive page bc im frustrated with the send page

--- ProjectTalon.App/ViewModel/TokenViewModel.cs
+++ ProjectTalon.App/ViewModel/TokenViewModel.cs
@@ -8,6 +8,7 @@ namespace ProjectTalon.App.ViewModel
 {
     public class TokenViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Asset { get; set; }
         public int Amount { get; set; }
