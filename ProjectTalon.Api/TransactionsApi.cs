2b7bbbb9ba8af22469937f873dd79b90601ebd9a
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2022-01-19 20:28:40 -0800

    Began work on transactions api

--- ProjectTalon.Api/TransactionsApi.cs
+++ ProjectTalon.Api/TransactionsApi.cs
@@ -1,9 +1,26 @@
-﻿namespace ProjectTalon.Api;
+﻿using Blockfrost.Api.Models;
+using Blockfrost.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTalon.Api;
 
 public class TransactionsApi
 {
     public static void AddEndpoints(WebApplication app)
     {
-        
+        app.MapGet("/blockfrost/transaction/{addresshash}/all", GetAllTransactions);
+    }
+
+    private static async Task<IResult> GetAllTransactions(IAddressesService addressesService,
+        string address)
+    {
+        try
+        {
+            return Results.Ok((await addressesService.GetTotalAsync(address)));
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
     }
 }
